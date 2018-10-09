e6ee4f621b85ade6b07baec424769d74b2fd0d2c
Taavi Päll <tapa741@gmail.com>
2018-10-10 00:31:49 +0300

    corrected response var

--- R/analyse.R
+++ R/analyse.R
@@ -4,27 +4,23 @@ library(brms)
 mudil <- read_csv("output/andmed.csv")
 mudil
 
-max(mudil$TW, na.rm = TRUE)
-sd(mudil$TW, na.rm = TRUE)
+max(mudil$TL, na.rm = TRUE)
+sd(mudil$TL, na.rm = TRUE)
 
 ggplot(data = mudil) +
   geom_point(mapping = aes(x = age, y = TL))
 
-prior1 <- prior(normal(160, 35), nlpar = "alpha") +
-  prior(normal(160, 35), nlpar = "beta") +
-  prior(normal(1, 2.5), nlpar = "k", lb = 0) +
-  prior(normal(1, 2.5), nlpar = "m", lb = 0)
-
-
-fit1 <- brm(bf(TW ~ (alpha^(1 - m) - beta * exp(-k * age)) ^ (1/(1 - m)), 
-               alpha + m + beta + k ~ 1, nl = TRUE),
+prior2 <- prior(normal(214, 42), nlpar = "Linf") +
+  prior(student_t(3, 0, 2), nlpar = "K", lb = 0) +
+  prior(student_t(3, 0, 2), nlpar = "t0", ub = 0)
+fit2 <- brm(bf(TL ~ Linf * (1 - exp(-K * (age - t0))), 
+               Linf + K + t0 ~ 1, nl = TRUE),
             data = mudil, 
-            prior = prior1,
+            prior = prior2,
             chains = 4,
             iter = 2400, 
-            control = list(adapt_delta = 0.999, max_treedepth = 15))
-write_rds(fit1, "output/von_bertalanffy_normal.rds")
+            control = list(adapt_delta = 0.999, max_treedepth = 13))
+write_rds(fit2, "output/von_bertalanffy_student_2.rds")
 
-
-summary(fit1)
-plot(marginal_effects(fit1), points = TRUE)
+summary(fit2)
+plot(marginal_effects(fit2), points = TRUE)
