58936d6c67c36a16b58e568563b924ba9356c32f
Taavi Päll <tapa741@gmail.com>
2018-10-09 19:53:27 +0300

    added model

--- R/analyse.R
+++ R/analyse.R
@@ -13,6 +13,7 @@ prior1 <- prior(normal(160, 35), nlpar = "alpha") +
   prior(normal(160, 35), nlpar = "beta") +
   prior(normal(1, 2.5), nlpar = "k", lb = 0) +
   prior(normal(1, 2.5), nlpar = "m", lb = 0)
+
 fit1 <- brm(bf(TW ~ (alpha^(1 - m) - beta * exp(-k * age)) ^ (1/(1 - m)), 
                alpha + m + beta + k ~ 1, nl = TRUE),
             data = mudil, 
@@ -20,5 +21,7 @@ fit1 <- brm(bf(TW ~ (alpha^(1 - m) - beta * exp(-k * age)) ^ (1/(1 - m)),
             chains = 1,
             iter = 2000, 
             control = list(adapt_delta = 0.999, max_treedepth = 15))
+write_rds(fit1, "output/von_bertalanffy_normal.rds")
+
 summary(fit1)
 plot(marginal_effects(fit1), points = TRUE)
