675171c599fa55ef44c42e350da8853a0c9c3db1
Taavi Päll <tapa741@gmail.com>
2018-10-26 19:05:05 +0300

    model update

--- R/analyse_otol.R
+++ R/analyse_otol.R
@@ -1,44 +1,115 @@
 
 library(tidyverse)
+library(viridis)
 library(brms)
 mudil <- read_csv("output/andmed_otoliit.csv")
 mudil
 
-max(mudil$tl, na.rm = TRUE)
-sd(mudil$tl, na.rm = TRUE)
+#' Fish id: location + nr
+mudil %>% 
+  group_by(nr, age, location) %>% 
+  summarise(N = n()) %>% 
+  filter(N > 1)
 
-ggplot(data = mudil) +
-  geom_point(mapping = aes(x = age, y = tl))
+mudil_mod <- mudil %>% 
+  mutate(location = str_replace_all(location, "\\s", "_"),
+         id = str_c(location, nr, sep = "_"),
+         sex = case_when(
+           sex == 0 ~ "F",
+           sex == 1 ~ "M",
+           sex == 3 ~ "juv"
+         )) %>% 
+  select(id, everything())
 
-prior2 <- prior(normal(212, 48), nlpar = "Linf") +
-  prior(student_t(3, 0, 2), nlpar = "K", lb = 0) +
-  prior(student_t(3, 0, 2), nlpar = "t0", ub = 0)
+ggplot(data = mudil_mod) +
+  geom_histogram(mapping = aes(x = tl), bins = 30) +
+  facet_wrap(~sex)
 
-fit <- brm(bf(tl ~ Linf * (1 - exp(-K * (age - t0))), 
+#' Only adult fish
+mudil_ad <- filter(mudil_mod, sex != "juv")
+
+mudil_ad %>% 
+  group_by(age) %>% 
+  summarise_at("tl", funs(mean, sd))
+
+#' Individual growth curves
+ggplot(data = mudil_ad) +
+  geom_line(mapping = aes(x = age, y = tl, group = id, color = sex)) +
+  facet_wrap(~location) +
+  scale_color_viridis_d()
+
+#' Average length at age in adults
+ggplot(data = mudil_ad, mapping = aes(x = age, y = tl)) +
+  stat_summary(fun.data = mean_sdl, fun.args = list(mult = 1), geom = "ribbon", alpha = 0.3) +
+  geom_point() +
+  stat_summary(fun.y = mean, geom = "line", color = viridis(6)[6]) +
+  facet_wrap(~location) +
+  labs(x = "Age (year)", y = "Total length (mm)")
+
+#' Starting values for van bertalaffny model coefficients
+library(FSA)
+svTypical <- vbStarts(tl ~ age, data = mudil_ad)
+unlist(svTypical)
+
+# Set up prior with suggested starting values using normal distribution
+prior0 <- prior(normal(168, 30), nlpar = "Linf") +
+  prior(normal(0.68, 0.2), nlpar = "K") +
+  prior(normal(0.5, 0.2), nlpar = "t0")
+
+get_prior(bf(tl ~ Linf * (1 - exp(-K * (age - t0))), 
+             Linf + K + t0 ~ 1, nl = TRUE),
+          data = mudil_ad)
+
+fit0 <- brm(bf(tl ~ Linf * (1 - exp(-K * (age - t0))), 
                Linf + K + t0 ~ 1, nl = TRUE),
-            data = mudil, 
-            prior = prior2,
+            data = mudil_ad,
+            family = gaussian(link = "log"),
+            prior = prior0,
             chains = 4,
-            iter = 2400)
-write_rds(fit, "output/von_bertalanffy_student_otol.rds")
-
-
-fit <- read_rds("output/von_bertalanffy_student_otol.rds")
-summary(fit)
-plot(marginal_effects(fit), points = TRUE)
-
-
-# t0 is almost 0, let's remove t0 from model
-prior0 <- prior(normal(212, 48), nlpar = "Linf") +
-  prior(student_t(3, 0, 2), nlpar = "K", lb = 0)
-fit0 <- brm(bf(tl ~ Linf * (1 - exp(-K * (age))), 
-              Linf + K ~ 1, nl = TRUE),
-           data = mudil, 
-           prior = prior0,
-           chains = 4,
-           iter = 2400)
-fit_file <- "output/von_bertalanffy_student_otol_0.rds"
-write_rds(fit0, fit_file)
-fit0 <- read_rds(fit_file)
+            iter = 4000)
+write_rds(fit0, "output/von_bertalanffy_normal_otol.rds")
+fit0 <- read_rds("output/von_bertalanffy_normal_otol.rds")
 summary(fit0)
-plot(marginal_effects(fit0), points = TRUE)
+plot(marginal_effects(fit0, method = "fitted"), points = TRUE)
+
+
+#' Model with individual variance
+
+get_prior(bf(tl ~ Linf * (1 - exp(-K * (age - t0))), 
+             Linf + K + t0 ~ 1 + (1 | id), nl = TRUE),
+          data = mudil_ad)
+
+fit1 <- brm(bf(tl ~ Linf * (1 - exp(-K * (age - t0))), 
+               Linf + K + t0 ~ 1 + (1 | id), nl = TRUE),
+            data = mudil_ad,
+            family = gaussian(link = "log"),
+            prior = prior0,
+            chains = 1,
+            iter = 4000)
+fit_file <- "output/von_bertalanffy_normal_otol_1.rds"
+write_rds(fit1, fit_file)
+fit1 <- read_rds(fit_file)
+summary(fit1)
+plot(marginal_effects(fit1), points = TRUE)
+
+
+get_prior(bf(tl ~ Linf * (1 - exp(-K * (age - t0))), 
+             Linf + K + t0 ~ 1 + location + (1 | gr(id, by = location)), nl = TRUE),
+          data = mudil_ad)
+
+fit2 <- brm(bf(tl ~ Linf * (1 - exp(-K * (age - t0))), 
+               Linf + K + t0 ~ 1 + location + (1 | gr(id, by = location)), nl = TRUE),
+            data = mudil_ad,
+            family = gaussian(link = "log"),
+            prior = prior0,
+            chains = 1,
+            iter = 4000)
+fit_file <- "output/von_bertalanffy_normal_otol_2.rds"
+write_rds(fit2, fit_file)
+fit2 <- read_rds(fit_file)
+summary(fit2)
+plot(marginal_effects(fit2), points = TRUE)
+
+cond <- make_conditions(data.frame(location = unique(mudil_ad$location)), vars = "location")
+plot(marginal_effects(fit2, conditions = cond), points = TRUE)
+
